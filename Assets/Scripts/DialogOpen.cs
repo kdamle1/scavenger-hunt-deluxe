5ceda2f1e172cd0198304aca90c5aace12dc5ae1
kdamle1 <karunadamle89@gmail.com>
2024-06-11 17:19:11 -0400

    Initial commit

---
+++ Assets/Scripts/DialogOpen.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogOpen : MonoBehaviour
+{
+
+    public string dialog;
+    public GameObject interfaceManager;
+    public PlayerHolding pHolding;
+    public bool begin = true;
+    public bool end = false;
+    private string[] collectibles;
+    private int clue;
+
+    private AudioSource greeting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        greeting = GetComponent<AudioSource>();
+        collectibles = new string[] { "film", "balloons", "life saver", "bull's eye", "pipe", "key", "fish", "birdhouse", "red airhorn", "magic hat" };
+        createClue();
+
+    }
+
+    public void createClue()
+    {
+        clue = Random.Range(0, 9);
+        searchDialog();
+    }
+
+    public void searchDialog()
+    {
+        dialog = "Hi! Can you help me find my " + collectibles[clue] + "?";
+
+        if (collectibles[clue] == "film")
+        {
+            dialog += "\n It was my favorite movie!";
+        }
+
+        if (collectibles[clue] == "balloons")
+        {
+            dialog += "\n My friend is having a party today!";
+        }
+
+        if (collectibles[clue] == "life saver")
+        {
+            dialog += "\n I have swim practice later today.";
+        }
+
+        if (collectibles[clue] == "bull's eye")
+        {
+            dialog += "\n We have archery practice later today!";
+        }
+
+        if (collectibles[clue] == "pipe")
+        {
+            dialog = "\n My grandfather was looking for it earlier.";
+        }
+
+        if (collectibles[clue] == "key")
+        {
+            dialog = "\n I was locked out of my house!";
+        }
+
+        if (collectibles[clue] == "fish")
+        {
+            dialog = "\n I need to feed it still!";
+        }
+
+        if (collectibles[clue] == "birdhouse")
+        {
+            dialog = "\n The birds will be waiting for me to give them food!";
+        }
+
+        if (collectibles[clue] == "red airhorn")
+        {
+            dialog = "\n I want to use it at the festival tonight!";
+        }
+
+        if (collectibles[clue] == "magic hat")
+        {
+            dialog = "\n I am performing at the magic show tonight!";
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!begin && pHolding.Verify())
+        {
+            checkClue();
+        }
+        greeting.Play();
+        InterfaceManager im = interfaceManager.GetComponent<InterfaceManager>();
+        im.ShowBox(dialog, clue);
+    }
+
+    private void checkClue()
+    {
+        if (pHolding.holdValue == clue)
+        {
+            dialog = "You found my " + collectibles[clue] + "! Hooray!";
+            end = true;
+        }
+        else
+        {
+            dialog = "No, that's not my " + collectibles[clue] + ".";
+        }
+    }
+
+    public void coinsScattered()
+    {
+        begin = false;
+    }
+
+}
---
+++ Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public TMP_Text dialogText;
+    public Image seekImage;
+    public GameObject npc;
+    public GameObject randomSpawn;
+
+    public Image collectible;
+    public GameObject showSprite;
+
+    [SerializeField]
+    private Sprite[] collectibleSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogBox.SetActive(false);
+        showSprite.SetActive(false);
+        dialogText.text = "Find this ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Submit") && dialogBox.activeInHierarchy == true)
+        {
+            dialogBox.SetActive(false);
+
+            if (npc.GetComponent<DialogOpen>().end)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    public void CollectibleUpdate(int item)
+    {
+        showSprite.SetActive(true);
+        collectible.GetComponent<Image>().sprite = collectibleSource[item];
+    }
+
+    public void ShowBox(string dialog, int item)
+    {
+        dialogBox.SetActive(true);
+        dialogText.text = dialog;
+        seekImage.GetComponent<Image>().sprite = collectibleSource[item];
+
+        if (npc.GetComponent<DialogOpen>().begin)
+        {
+            scatterCoins();
+        }
+    }
+
+    public void scatterCoins()
+    {
+        randomSpawn.GetComponent<RandomSpawn>().DistributeCollectibles();
+        npc.GetComponent<DialogOpen>().coinsScattered();
+    }
+}
